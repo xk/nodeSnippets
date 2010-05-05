b763cfdbe9c5bca4e85a5e717495f6e0cbf10d02
Ry Nohryb <jorge@jorgechamorro.com>
2010-05-05 19:15:47 +0200

    grep.js : arreglar locales

--- grep.js
+++ grep.js
@@ -6,7 +6,8 @@ var sys= require("sys");
 var stdin= process.openStdin();                           //Abrimos stdin que es por donde nos llegan los datos
 stdin.setEncoding('utf8');                                //esperemos que en utf-8
 
-stdin.addListener('data', function (chunk, newLinePos) {  //los datos van llegando por aquí
+var buffer= "";
+stdin.addListener('data', function (chunk) {              //los datos van llegando por aquí
   buffer+= chunk;                                         //Se almacenan en el buffer de entrada
   process.nextTick(procesarBuffer);                       //Y se procesan "en los ratos libres"
 });
@@ -19,10 +20,9 @@ stdin.addListener('end', function () {                    //sacabó
 
 var ER= new RegExp(process.argv[2], "g");                 //La RegExp que nos han pasado
 var ctr= 0;
-var buffer= "";
-function procesarBuffer () {                              //Esto se ejecuta mientras estamos esperando a que lleguen más datos
+function procesarBuffer (newLinePos) {                    //Esto se ejecuta mientras estamos esperando a que lleguen más datos
   while ((newLinePos= buffer.indexOf("\n")) >= 0) {       //Vamos cortando por los saltos de línea: esto es una especie de readLine()
-    ctr+= ER.test(buffer.substr(0, newLinePos));          //al sumar true/false a un 'number' se convierte en 1/0
+    ctr+= ER.test(buffer.substr(0, newLinePos));
     buffer= buffer.substr(newLinePos+1);
   }
 }
