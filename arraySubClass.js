6dde71082b723acf8b82985171d959dbe331b3a0
Ry Nohryb <jorge@jorgechamorro.com>
2010-05-05 13:43:33 +0200

    añadida arraySubClass

---
+++ arraySubClass.js
@@ -0,0 +1,23 @@
+//#!/usr/bin/env node
+//20100505 jorge@jorgechamorro.com
+
+var global= Function("return this")();
+var sys= (require && require("sys")) || {};
+var puts= (global.console && console.log && (function (p) { console.log(p) })) || sys.puts || global.print || global.alert;
+var inspect= sys.inspect || JSON.stringify;
+
+var clase= { foo: function () {
+  return inspect(this);
+}};
+
+var array= [1,2];
+array.__proto__= clase;
+clase.__proto__= Array.prototype;
+
+array.push(3);
+puts(array.foo());
+puts("array.length:"+ array.length);
+array[5]= 5;
+puts("array.length:"+ array.length);
+puts("array instanceof Array: "+ (array instanceof Array));
+puts(array.foo());
