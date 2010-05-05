40bb30d8fe8d7acf8e285c275df9cb4c69856571
Ry Nohryb <jorge@jorgechamorro.com>
2010-05-05 22:29:00 +0200

    apaño Darío

--- grep2.js
+++ grep2.js
@@ -5,3 +5,11 @@ require("child_process").exec(
   "cat "+ process.argv[2]+ " | grep '"+ process.argv[3]+ "' | wc -l",
   function (err, stdout, stderr) { require("sys").puts(stdout); }
 );
+
+/*
+Gasta menos procesador:
+require("child_process").exec(
+  "grep -c '"+ process.argv[3]+ "' "+ process.argv[2],
+  function (err, stdout, stderr) { require("sys").puts(stdout); }
+);
+*/
