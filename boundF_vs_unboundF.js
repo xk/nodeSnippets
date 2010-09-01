bfb83b678c361166457e7e7f91cfd009a7ef6ea5
Ry Nohryb <jorge@jorgechamorro.com>
2010-09-01 04:13:45 +0200

    touched bound vs unbound function call benchmark

--- boundF_vs_unboundF.js
+++ boundF_vs_unboundF.js
@@ -3,20 +3,20 @@ var kMax= 1e4;
 function un_bound (a) { return a+ Math.random() }
 var bound= un_bound.bind({});
 
-var acummulator= 0;
+var acumulador= 0;
 
 function a () {
+var n= kMax;
   var t= Date.now();
-  var n= kMax;
-  while (n--) acummulator+= un_bound();
+  while (n--) acumulador+= un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0)+ un_bound(0);
   a.ms+= (Date.now()- t);
   a.ctr++;
 }
 
 function b () {
+var n= kMax;
   var t= Date.now();
-  var n= kMax;
-  while (n--) acummulator+= bound();
+  while (n--) acumulador+= bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0)+ bound(0);
   b.ms+= (Date.now()- t);
   b.ctr++;
 }
@@ -27,15 +27,15 @@ b.ctr= b.ms= 0;
 var tests= [a,b];
 
 setInterval(function () {
-  var n= 0, txt= "\r", loops= [], tAcum= [];
+  var n= 0, txt= "\r",  loops= [],  tAcum= [];
   while (n < tests.length) {
    var f= tests[n], name= f.name;
-   txt+= name+ ".ms: "+ (f.ms/f.ctr).toFixed(2)+ " ms, ";
+   txt+= name+ ": "+ (f.ms/f.ctr).toFixed(2)+ " ms  ";
    loops[n]= f.ctr;
    tAcum[n]= f.ms;
    n++;
   }
-  txt+= "loops: ["+ loops+ "], t acumulado: "+ tAcum;
+  txt+= "loops: ["+ loops+ "]  t acumulado: ["+ tAcum+ "]  acumulador: "+ acumulador;
   console.log(txt);
 }, 1e3);
 
