37692fb9037c79b22d2a4c9b01d0bd9ec4a31df7
Jorge Chamorro Bieling <jorge@jorgechamorro.com>
2010-10-11 15:07:22 +0200

    kk

--- nodeErrorsHowTo.js
+++ nodeErrorsHowTo.js
@@ -38,3 +38,25 @@ process.on('uncaughtException', function ƒ (err) {
   var stack= err.stack.split("\n");
   console.log('Caught exception:\n', inspect(stack,0,99,1,1), "\n", inspect(previous,0,99,1,1));
 });
+
+/*
+
+Caught exception:
+ [ 'Error: This is the error that we want to track'
+, '    at EventEmitter.emit (events:41:20)'
+, '    at node.js:599:9'
+] 
+ [ 'Error: previousTrace'
+, '    at g (/Users/jorge/JAVASCRIPT/nodeSnippets/nodeErrorsHowTo.js:7:20)'
+, '    at f (/Users/jorge/JAVASCRIPT/nodeSnippets/nodeErrorsHowTo.js:8:8)'
+, '    at e (/Users/jorge/JAVASCRIPT/nodeSnippets/nodeErrorsHowTo.js:9:6)'
+, '    at d (/Users/jorge/JAVASCRIPT/nodeSnippets/nodeErrorsHowTo.js:10:4)'
+, '    at Object.<anonymous> (/Users/jorge/JAVASCRIPT/nodeSnippets/nodeErrorsHowTo.js:11:2)'
+, '    at Module._compile (node.js:315:23)'
+, '    at Object..js (node.js:323:12)'
+, '    at Module.load (node.js:250:25)'
+, '    at Object.runMain (node.js:337:24)'
+, '    at Array.0 (node.js:587:12)'
+]
+
+*/
