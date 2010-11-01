9f3fb3eff02ffa17273359b29875d52c436bdc72
Jorge Chamorro Bieling <jorge@jorgechamorro.com>
2010-11-01 15:55:39 +0100

    scrncapture v2

--- screenCapture.js
+++ screenCapture.js
@@ -11,95 +11,82 @@ var fs= require('fs');
 var MAX_INT= Math.pow(2,53);
 var html= getNamedChunks(arguments.callee.toString()).srcHTML;
 var kPeriodoDeCaducidad= 33;
-var fifo1= "/var/tmp/___nOdEfIfO_"+ Date.now().toString(36)+ (MAX_INT * Math.random()).toString(36);
-var fifo2= "/var/tmp/___nOdEfIfO_"+ Date.now().toString(36)+ (MAX_INT * Math.random()).toString(36);
-exec('mkfifo '+ fifo1+ " "+ fifo2, function (error, stdout, stderr) {
-  if (error) throw error;
+var fifo1= process.ENV.TMPDIR+ "___nOdEfIfO_"+ Date.now().toString(36)+ (MAX_INT * Math.random()).toString(36);
+var fifo2= process.ENV.TMPDIR+ "___nOdEfIfO_"+ Date.now().toString(36)+ (MAX_INT * Math.random()).toString(36);
+exec('mkfifo '+ fifo1+ " "+ fifo2, function (e, o, i) {
+  if (e) throw e;
+  main();
 });
 
-var quitting;
-var capturaEnCurso;
-var ultimaCaptura;
-var cbQueue= [];
-var maxSize= 0;
-var minSize= 1e9;
+function main () {
+  var capturando;
+  var captura;
+  var cbQueue= [];
+  var maxSize= 0;
+  var minSize= 1e9;
 
-function capturarAhora () {
-  if (quitting || capturaEnCurso) return;
-  capturaEnCurso= true;
-  
-  var scrn= exec('screencapture -CSx -t jpg '+ fifo1, function dsp (e,o,i) {
-    if (e) throw e;
-    var imck= exec("convertt -quality 70 jpg:"+ fifo1+ "'[55%]' jpg:"+ fifo2, function dsp (e,o,i) {
-      dispatchQueue( e ? fifo1 : fifo2 );
-      function dispatchQueue (srcFile) {
-        fs.readFile(srcFile, function (e, data) {
-          if (e) throw e;
-          if (capturarAhora.caducaTimer) clearTimeout(capturarAhora.caducaTimer);
-          capturarAhora.caducaTimer= setTimeout(function () {
-            capturaEnCurso= false;
-            ultimaCaptura= null;
-          }, kPeriodoDeCaducidad);
-          ultimaCaptura= data;
-          if (data.length > maxSize) console.log('MaxSize -> '+ (maxSize= data.length));
-          if (data.length < minSize) console.log('MinSize -> '+ (minSize= data.length));
-          cbQueue.forEach(function (cb) {
-            cb(ultimaCaptura);
-          });
-          cbQueue= [];
-        });
-      }
-    });
-  });
-}
-
-function dameUnaCaptura (cb) {
-  if (ultimaCaptura) return cb(ultimaCaptura);
-  cbQueue.push(cb);
-  capturarAhora();
-}
-
-require('http').createServer(function (req, res) {
-  if (req.method !== 'GET') {
-    res.writeHead(200, {'Content-Type' : 'txt/plain'});
-    res.end('FAIL');
-  }
-  else if ((req.url.indexOf("screencapture") >= 0) || (req.url.indexOf("favicon") >= 0)) {
-    res.writeHead(200, {'Content-Type' : 'image/jpeg', 'Cache-Control' : 'no-cache'});
-    dameUnaCaptura(function (imagen) {
-      res.end(imagen);
-    });
-  }
-  else {
-    res.writeHead(200, {'Content-Type': 'text/html', 'Cache-Control' : 'no-cache'});
-    res.end(html, 'utf8');
-  }
-}).listen(12345);
-
-console.log('Server running at http://127.0.0.1:12345/');
-console.log(fifo1);
-console.log(fifo2);
-
-function exit () {
-  if (capturaEnCurso) {
-    quitting= true;
-    setTimeout(exit, 10);
-    return;
-  }
-  if (!exit.flag) {
-    exit.flag= 1;
-    exec('rm '+ fifo1, [], function (error, stdout, stderr) {
-      exec('rm '+ fifo2, [], function (error, stdout, stderr) {
-        console.log('\nBYE');
-        process.exit(0);
+  function capturarAhora () {
+    if (capturando) return;
+    capturando= true;
+    exec('screencapture -CSx -t jpg '+ fifo1, function (e,o,i) {
+      if (e) throw e;
+      exec("convert -quality 70 jpeg:"+ fifo1+ "'[55%]' jpeg:"+ fifo2, function (e,o,i) {
+        if (e) throw e;
+      });
+      fs.readFile(fifo2, function (e, data) {
+        if (e) throw e;
+        capturando= false;
+        captura= data;
+        cbQueue.forEach(function (cb) { cb(captura) });
+        cbQueue= [];
+        setTimeout(function () { captura= null }, kPeriodoDeCaducidad);
+        if ((data.length > maxSize) || (data.length < minSize)) {
+          if (data.length > maxSize) maxSize= data.length;
+          if (data.length < minSize) minSize= data.length;
+          console.log([minSize, maxSize]);
+        }
       });
     });
   }
+
+  function dameUnaCaptura (cb) {
+    if (captura) return cb(captura);
+    cbQueue.push(cb);
+    capturarAhora();
+  }
+
+  var server= require('http').createServer(conexion);
+  server.listen(12345);
+  
+  function conexion (req, res) {
+    if (req.method !== 'GET') {
+      res.writeHead(200, {'Content-Type' : 'txt/plain'});
+      res.end('FAIL');
+    }
+    else if ((req.url.indexOf("screencapture") >= 0) || (req.url.indexOf("favicon") >= 0)) {
+      res.writeHead(200, {'Content-Type' : 'image/jpeg', 'Cache-Control' : 'no-cache'});
+      dameUnaCaptura(function (imagen) {
+        res.end(imagen);
+      });
+    }
+    else {
+      res.writeHead(200, {'Content-Type': 'text/html', 'Cache-Control' : 'no-cache'});
+      res.end(html, 'utf8');
+    }
+  }
+
+  console.log('Server running at http://127.0.0.1:12345/');
+  console.log(fifo1);
+  console.log(fifo2);
+
+  function exit () {
+    exec('rm '+ fifo1+ " "+ fifo2, function (e,o,i) {});
+    console.log('\nBYE');
+  }
+
+  process.on('SIGINT', exit);
 }
 
-process.on('exit', exit);
-process.on('SIGINT', exit);
-process.on('uncaughtException', exit);
 
 /* beginchunk: srcHTML
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
@@ -114,7 +101,6 @@ process.on('uncaughtException', exit);
   <!-- Date: 2010-10-26 -->
   <style type="text/css">
     body { margin:0; padding:0; }
-    #imgcontainer { padding:0; }
   </style>
   <script type="text/javascript">
     window.onload= function () {
@@ -138,9 +124,7 @@ process.on('uncaughtException', exit);
   </script>
 </head>
 <body>
-  <div id="imgcontainer">
-    <img id="img">
-  </div>
+  <img id="img">
 </body>
 </html>
 endchunk */
@@ -149,16 +133,18 @@ function getNamedChunks (txt) {
   var etiq;
   var chunks= {};
   txt= txt.split('\n');
+  var endRE= /^endchunk[\s]{0,}\*\/$/i;
+  var beginRE= /^\/\*[\s]{0,}beginchunk:[\s]{0,}([^\s]{1,})[\s]{0,}$/i;
   txt.forEach(function (v,i,o) {
     var r;
     if (etiq) {
-      if ((/^endchunk[\s]{0,}\*\/$/i).test(v)) {
+      if (endRE.test(v)) {
         chunks[etiq]= chunks[etiq].join('\n');
         etiq= '';
       }
       else chunks[etiq].push(v);
     }
-    else if (r= v.match(/^\/\*[\s]{0,}beginchunk:[\s]{0,}([^\s]{1,})[\s]{0,}$/i)) {
+    else if (r= v.match(beginRE)) {
       etiq= r[1];
       chunks[etiq]= [];
     }
