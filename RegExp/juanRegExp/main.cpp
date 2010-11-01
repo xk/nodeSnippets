f4f2d808846b4b6a36962cd8c0c1cd0eb718a6bb
Jorge Chamorro Bieling <jorge@jorgechamorro.com>
2010-11-01 14:30:22 +0100

    C++ vs JS : regexp s/\t/ /g

--- RegExp/juanRegExp/main.cpp
+++ RegExp/juanRegExp/main.cpp
@@ -13,30 +13,30 @@ int matchstar(int c, char *regexp, char *text);
 
 /* match: search for regexp anywhere in text */
 int match(char *regexp, char *text) {
-	if (regexp[0] == '^') return matchhere(regexp+1, text);
-	do {	/* must look even if string is empty */
-		if (matchhere(regexp, text))
-			return 1;
-	} while (*text++ != '\0');
-	return 0;
+  if (regexp[0] == '^') return matchhere(regexp+1, text);
+  do {  /* must look even if string is empty */
+    if (matchhere(regexp, text))
+      return 1;
+  } while (*text++ != '\0');
+  return 0;
 }
 
 /* matchhere: search for regexp at beginning of text */
 int matchhere(char *regexp, char *text) {
-	if (regexp[0] == '\0') return 1;
-	if (regexp[1] == '*') return matchstar(regexp[0], regexp+2, text);
-	if (regexp[0] == '$' && regexp[1] == '\0') return *text == '\0';
-	if (*text!='\0' && (regexp[0]=='.' || regexp[0]==*text))
-		return matchhere(regexp+1, text+1);
-	return 0;
+  if (regexp[0] == '\0') return 1;
+  if (regexp[1] == '*') return matchstar(regexp[0], regexp+2, text);
+  if (regexp[0] == '$' && regexp[1] == '\0') return *text == '\0';
+  if (*text!='\0' && (regexp[0]=='.' || regexp[0]==*text))
+    return matchhere(regexp+1, text+1);
+  return 0;
 }
 
 /* matchstar: search for c*regexp at beginning of text */
 int matchstar(int c, char *regexp, char *text) {
-	do {	/* a * matches zero or more instances */
-		if (matchhere(regexp, text)) return 1;
-	} while (*text != '\0' && (*text++ == c || c == '.'));
-	return 0;
+  do {  /* a * matches zero or more instances */
+    if (matchhere(regexp, text)) return 1;
+  } while (*text != '\0' && (*text++ == c || c == '.'));
+  return 0;
 }
 
 
@@ -44,10 +44,10 @@ int matchstar(int c, char *regexp, char *text) {
 
 int main(int argc, char *argv[])
 {
-	if (argc < 3) {
-		cerr << "Error:: usage:" << argv[0] << " regexp text" << endl ;
-		return 1;
-	}
-	cout << match(argv[1], argv[2]) << endl;
-	return 0;
+  if (argc < 3) {
+    cerr << "Error:: usage:" << argv[0] << " regexp text" << endl ;
+    return 1;
+  }
+  cout << match(argv[1], argv[2]) << endl;
+  return 0;
 }
--- RegExp/regexp.js
+++ RegExp/regexp.js
@@ -8,37 +8,37 @@ function match (regexp, txt) {
   var txtPtr= 0, regexpPtr= 0;
   console.log("input -> "+ [regexp, txt]);
   
-	if (regexp[0] === '^') return matchhere(regexpPtr+1, txtPtr);
-	do {	/* must look even if string is empty */
-		if (matchhere(regexpPtr, txtPtr)) return 1;
-	} while (txt[++txtPtr]);
-	return 0;
+  if (regexp[0] === '^') return matchhere(regexpPtr+1, txtPtr);
+  do {  /* must look even if string is empty */
+    if (matchhere(regexpPtr, txtPtr)) return 1;
+  } while (txt[++txtPtr]);
+  return 0;
 
   /* matchhere: search for regexp at beginning of text */
   function matchhere (regexpPtr, txtPtr) {
-  	if (!regexp[regexpPtr]                                   ) return 1;
-  	if ( regexp[regexpPtr+1] === '*'                         ) return matchstar(regexp[regexpPtr], regexpPtr+2, txtPtr);
-  	if ( regexp[regexpPtr]   === '$' && !regexp[regexpPtr+1] ) return +!txt[txtPtr];
-  	if (txt[txtPtr] && (regexp[regexpPtr] === '.' || regexp[regexpPtr] === txt[txtPtr])) {
-  	  return matchhere(regexpPtr+1, txtPtr+1);
-  	}
-  	return 0;
+    if (!regexp[regexpPtr]                                   ) return 1;
+    if ( regexp[regexpPtr+1] === '*'                         ) return matchstar(regexp[regexpPtr], regexpPtr+2, txtPtr);
+    if ( regexp[regexpPtr]   === '$' && !regexp[regexpPtr+1] ) return +!txt[txtPtr];
+    if (txt[txtPtr] && (regexp[regexpPtr] === '.' || regexp[regexpPtr] === txt[txtPtr])) {
+      return matchhere(regexpPtr+1, txtPtr+1);
+    }
+    return 0;
   }
 
   /* matchstar: search for c*regexp at beginning of text */
   function matchstar (c, regexpPtr, txtPtr) {
-  	do {	/* a * matches zero or more instances */
-  		if (matchhere(regexpPtr, txtPtr)) return 1;
-  	} while (txt[txtPtr] && (txt[++txtPtr] === c || c === '.'));
-  	return 0;
+    do {  /* a * matches zero or more instances */
+      if (matchhere(regexpPtr, txtPtr)) return 1;
+    } while (txt[txtPtr] && (txt[++txtPtr] === c || c === '.'));
+    return 0;
   }
 }
 
 function main (argc, argv) {
-	if (argc < 4) {
+  if (argc < 4) {
     console.error("Error:: usage: "+ argv[1]+ " regexp text");
-		return 1;
-	}
+    return 1;
+  }
 
   console.log(match(argv[2], argv[3]));
   return 0;
