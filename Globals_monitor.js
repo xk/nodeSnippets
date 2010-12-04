943feed7aa8a17117ef37add36ad720206ea8316
Jorge Chamorro Bieling <jorge@jorgechamorro.com>
2010-12-04 15:58:14 +0100

    kk

--- Globals_monitor.js
+++ Globals_monitor.js
@@ -2,7 +2,7 @@
   var global= (function(){return this})();
   var seen= Object.getOwnPropertyNames(global);
   (function monitorGlobals () {
-    var nu= Object.getOwnPropertyNames(window);
+    var nu= Object.getOwnPropertyNames(global);
     if (nu.length !== seen.length) {
       nu.forEach(function (key,unused1,unused2) {
         if (seen.indexOf(key) < 0) console.error("*** WARNING: New global symbol -> '"+ key+ "' -> "+ global[key]);
