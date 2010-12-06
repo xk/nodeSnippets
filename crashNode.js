7b5d751c2b5c9965ad723ba912dc639b45411792
Jorge Chamorro Bieling <jorge@jorgechamorro.com>
2010-12-06 18:16:43 +0100

    varias cosas

--- crashNode.js
+++ crashNode.js
@@ -1,6 +1,6 @@
 var ctr= 0;
-var t0= Date.now();
-while (t0 === Date.now()) ; //get in sync with clock
+var t0= Date.now()+ 2;
+while (t0 < Date.now()) ; //get in sync with clock
 
 (function dspCtr () {
   console.log(["#", ctr, " -> ", Date.now()-t0, "ms"].join(''));
