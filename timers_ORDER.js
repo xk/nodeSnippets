dd9822d5c657608754fcd576b79fef2e8b26908c
Jorge Chamorro Bieling <jorge@jorgechamorro.com>
2011-06-02 19:51:23 +0200

    timers order test v2

--- timers_ORDER.js
+++ timers_ORDER.js
@@ -7,7 +7,6 @@ To demostrate that node.js is the only JS platform that fires timers in an unpre
 
 var ctr= 0;
 var RUN= 1;
-var res= [];
 var kMax= 1024;
 var isaBrowser= !!(function(){return this})().window;
 
@@ -18,14 +17,10 @@ function run () {
 function f (ms) {
   return function () {
     if (RUN) {
-      var pos= res.length;
-      RUN= (pos === ms);
-      var txt= ['#'+ (ctr++),pos,ms,RUN?'OK':'BAD'];
+      RUN= (ms === (ctr % kMax));
+      var txt= ['#'+ctr,ms,RUN?'OK':'BAD'];
       isaBrowser? document.body.innerHTML= txt : console.log(txt);
-      if (res.push(ms) >= kMax) {
-        res.length= 0;
-        run();
-      }
+      !(++ctr % kMax) && run();
     }
   };
 }
@@ -36,7 +31,7 @@ run();
 
 To test a browser use this bookmarklet:
 
-javascript:var ctr= 0;var RUN= 1;var res= [];var kMax= 1024;var isaBrowser= !!(function(){return this})().window;function run () { for (var ms=0; ms<kMax; ms++) setTimeout(f(ms),ms);}function f (ms) { return function () { if (RUN) { var pos= res.length; RUN= (pos === ms); var txt= ['#'+ (ctr++),pos,ms,RUN?'OK':'BAD']; isaBrowser? document.body.innerHTML= txt : console.log(txt); if (res.push(ms) >= kMax) { res.length= 0; run(); } } };}run();
+javascript:var ctr= 0;var RUN= 1;var kMax= 1024;var isaBrowser= !!(function(){return this})().window;function run () { for (var ms=0; ms<kMax; ms++) setTimeout(f(ms),ms);}function f (ms) { return function () { if (RUN) { RUN= (ms === (ctr % kMax)); var txt= ['#'+ctr,ms,RUN?'OK':'BAD']; isaBrowser? document.body.innerHTML= txt : console.log(txt); !(++ctr%kMax) && run(); } };}run();
 
 */
 
