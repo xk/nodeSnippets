aff3e382138adb29a61bf1ab033afcf6fce4e427
Jorge Chamorro Bieling <jorge@jorgechamorro.com>
2011-11-02 20:12:11 +0100

    mp3 duration

--- mp3_duration.js
+++ mp3_duration.js
@@ -6,7 +6,5 @@ var file= '/test.m4a';
 require('child_process').exec('ffmpeg -i '+ file, cb);
 
 function cb (error, stdout, stderr) {
-  stdout+= stderr;
-  stdout= stdout.split('Duration: ')[1].split(', start: ')[0];
-  console.log('Duration: '+ stdout);
+  console.log('Duration: '+ (stdout + stderr).split('Duration: ')[1].split(', start: ')[0]);
 }
